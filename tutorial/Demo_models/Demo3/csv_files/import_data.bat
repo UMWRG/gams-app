706b8ebfb15eb7fa4906ca88277a81e06bb48479
khaled <khaledk2@hotmail.com>
2015-01-16 19:01:08 +0000

    Change template file name to be the same as in the batch file change 'dimensionless' to be 'Dimensionless' for supply node

--- tutorial/Demo_models/Demo3/csv_files/import_data.bat
+++ tutorial/Demo_models/Demo3/csv_files/import_data.bat
@@ -1,2 +1,2 @@
-python F:\work\HydraPlatform\HydraPlugins\CSVplugin\ImportCSV\ImportCSV.py -t network.csv -n nodes.csv -l links.csv -g groups.csv -k group_members.csv -x -m tutorial_template.xml 
+python ~/git/HYDRA/HydraPlugins/CSVplugin/trunk/ImportCSV/\ImportCSV.py -t network.csv -n nodes.csv -l links.csv -g groups.csv -k group_members.csv -x -m tutorial_template.xml 
 pause
