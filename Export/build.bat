7d547b5f24b3dbcdbb290970ae4c1d7a401c198a
Stephen Knox <stephen.knox@manchester.ac.uk>
2015-01-18 12:00:00 +0000

    Commit gams plugin that looks specifically for hydra.ini only

--- Export/build.bat
+++ Export/build.bat
@@ -1,4 +1,4 @@
-pyinstaller --upx-dir=../../upx GAMSExport.spec
+pyinstaller -y --upx-dir=../../upx GAMSExport.spec
 xcopy dist\GAMSExport\* ..\GAMSPlugin\plugins\GAMSExport\ /Y /s
 copy plugin.xml ..\GAMSPlugin\plugins\GAMSExport\plugin.xml /Y
 pause
