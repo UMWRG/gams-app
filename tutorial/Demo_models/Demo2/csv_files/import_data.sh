7a052c6035178a018b7683974cae6bef9b5a97db
stephen <s.knox@ucl.ac.uk>
2015-01-26 14:34:52 +0000

    Update the data in demo2 to match the template correctly and to be a bit more clear

--- tutorial/Demo_models/Demo2/csv_files/import_data.sh
+++ tutorial/Demo_models/Demo2/csv_files/import_data.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
-python ~/git/HYDRA/HydraPlugins/CSVplugin/ImportCSV/ImportCSV.py -t network.csv -n nodes.csv -l links.csv -g groups.csv -k group_members.csv -x -m tutorial_template.xml 
+if [ -n $1 ]; then
+python ~/git/HYDRA/HydraPlugins/CSVplugin/ImportCSV/ImportCSV.py -t network.csv -n nodes.csv -l links.csv -g groups.csv -k group_members.csv -x -m template.xml
+else
+python ~/git/HYDRA/HydraPlugins/CSVplugin/ImportCSV/ImportCSV.py -i $1 -s "$2" -t network.csv -n nodes.csv -l links.csv -g groups.csv -k group_members.csv -x -m template.xml
+fi
