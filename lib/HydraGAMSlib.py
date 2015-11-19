9345c1426baa98c3af33c698ba14d5c5abda10e9
khaled <khaledk2@hotmail.com>
2015-11-19 09:41:04 +0000

    Wrapping importing modules from gams in try catch a

--- lib/HydraGAMSlib.py
+++ lib/HydraGAMSlib.py
@@ -26,13 +26,16 @@ class GamsModel(object):
     def __init__(self, gamspath, working_directory):
         if(gamspath==None):
             gamspath=get_gams_path()
+        try:
+            real_path = os.path.realpath(os.path.abspath(gamspath))
+            api_path = os.path.join(real_path,'apifiles','Python','api')
+            if api_path not in sys.path:
+                sys.path.insert(0, api_path)
+            from gams import  workspace
+            self.ws = workspace.GamsWorkspace(working_directory=working_directory, system_directory=gamspath, debug = 1)
 
-        real_path = os.path.realpath(os.path.abspath(gamspath))
-        api_path = os.path.join(real_path,'apifiles','Python','api')
-        if api_path not in sys.path:
-            sys.path.insert(0, api_path)
-        from gams import  workspace
-        self.ws = workspace.GamsWorkspace(working_directory=working_directory, system_directory=gamspath, debug = 1)
+        except Exception as e:
+            raise HydraPluginError("Unable to import modules from gams. Please ensure that gams with version greater than 24.1 is installed.")
 
     def add_job(self, model_file):
        """
