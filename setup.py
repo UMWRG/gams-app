7960a09758cdff19bf0aa8a760e242aa12f46dba
Stephen Knox <knoxsp@gmail.com>
2018-11-21 16:21:07 +0000

    Add dependencies

--- setup.py
+++ setup.py
@@ -2,11 +2,16 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='hydra-python',
+    name='hydra-gams',
     version='0.1',
     description='Hydra client applications and library for importing hydra network to gams-compatible input files, running gams model and importing results contained int the GDX file format.',
     packages=find_packages(),
     include_package_data=True,
+    install_requires=[
+        'click',
+        'hydra-base',
+        'hydra-client-python',
+    ],
     entry_points='''
     [console_scripts]
     hydra-gams=hydra_gams.cli:start_cli
